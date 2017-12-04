f5b67b83275d3a7e862e7331321bbc3eff8235af
Marcus Gutierrez <marcus.gutierrez@live.com>
2017-12-04 03:06:30 -0700

    Removed User Table

---
+++ database/migrations/2017_12_04_092403_update_stamp_foreign.php
@@ -0,0 +1,48 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class UpdateStampForeign extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::disableForeignKeyConstraints();
+        Schema::dropIfExists('users');
+        Schema::table('sessions', function (Blueprint $table){
+            $table->dropForeign('sessions_user_id_foreign');
+            $table->foreign('user_id')
+                    ->references('id')->on('turk_user')
+                    ->onDelete('cascade');
+        });
+        Schema::table('section_stamp', function (Blueprint $table) {
+            $table->dropForeign('section_stamp_user_id');
+            $table->foreign('user_id')
+                    ->references('id')->on('turk_user')
+                    ->onDelete('cascade');
+        });
+        Schema::table('answer', function (Blueprint $table) {
+            $table->dropForeign('answer_user_id_foreign');
+            $table->foreign('user_id')
+                    ->references('id')->on('turk_user')
+                    ->onDelete('cascade');
+        });
+        Schema::enableForeignKeyConstraints();
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        //
+    }
+}
