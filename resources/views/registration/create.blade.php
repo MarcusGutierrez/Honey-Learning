8acccbfd65505d150ad34c7895d1f45b8b985edc
Marcus Gutierrez <marcus.gutierrez@live.com>
2017-12-09 18:27:09 -0700

    Updated payment description v2.0

--- resources/views/registration/create.blade.php
+++ resources/views/registration/create.blade.php
@@ -21,9 +21,9 @@
 
     <p style="text-align: justify; text-justify: inner-word; font-size: 1.1em;">
         You make decisions for 50 rounds of the game. After completing the game, you
-        will also be asked to answer a short questionnaire. This study is expected 
-        to take 30 minutes.  You will be paid $1.50 only for fully completing 
-        the experiment and $1.00 for completing the 2 post-game surveys. You 
+        will also be asked to answer 2 short questionnaires. This study is expected 
+        to take 30 minutes.  You will be paid $1.50 only for fully completing full
+        the experiment and $1.00 for completing the 2 post-game questionnaires. You 
         will also be awarded $0.01 for every point accumulated in the game.  
         Every point counts, and thus, you need to pay attention to every decision 
         while aiming to avoid honeypots.
